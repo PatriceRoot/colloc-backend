e3518ebadc51a5d0cabad458d24ef4505708b33e
PatriceRoot <patricekalwira@gmail.com>
2024-08-04 14:02:57 +0200

    ajout du message d'erreur lors de la connexion

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,6 +8,11 @@ export const registerUser = async (req, res) => {
   const { email, password, name } = req.body;
 
   try {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
       data: {
